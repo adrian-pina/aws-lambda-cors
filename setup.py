24c1cf663a2776e9c29123f87e2ed2a92990e2ef
Adrian Piña <adrianxplay@gmail.com>
2020-02-09 17:37:38 -0600

    fix package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="aws-lambda-cors",
+    name="aws_lambda_cors",
     version="0.0.2",
     author="Adrian Pina",
     author_email="adrian@quesadillalab.mx",
